79642df9a5416b0c36e6ac1dfc69917b375748eb
Musbi <musbi700@gmail.com>
2025-07-07 21:48:27 +0100

    settings file added

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """A class to manage the game settings
+    """
+
+    def __init__(self):
+        """Initialize the settings attributes
+        """
+
+        self.sreen_width = 800
+        self.screen_height = 500
+        self.bg_color = (24, 60, 11)
+        self.game_title = "Star Grid"
