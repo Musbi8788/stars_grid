6ad53991e7878b22e15e2594f5b7a9a399d7cd89
Musbi <musbi700@gmail.com>
2025-07-07 22:38:29 +0100

    settings up to date

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings():
         """Initialize the settings attributes
         """
 
-        self.sreen_width = 800
+        self.screen_width = 800
         self.screen_height = 500
         self.bg_color = (24, 60, 11)
         self.game_title = "Star Grid"
